df7f51e68c86acb8a18c89d6f657fe0969d5f63f
Holly McGuinness <gaithelema@gmail.com>
2022-08-02 17:57:40 -0400

    first commit

---
+++ Client/main.js
@@ -0,0 +1,10 @@
+function rotateFunction(){
+    var min = 1024;
+    var max = 9999;
+    var deg = Math.floor(Math.random() * (max - min)) + min;
+    document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+  }
+
+  setTimeout(function(){
+    element.classList.add('animate');
+  }, 5000);

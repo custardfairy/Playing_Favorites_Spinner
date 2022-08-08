fc718f2b31e282f72f48825804c9b6ba558410a3
Holly McGuinness <gaithelema@gmail.com>
2022-08-08 15:42:02 -0400

    axios updates

---
+++ Server/controller.js
@@ -0,0 +1,5 @@
+const bgaBtn = document.getElementById("bgaBtn");
+bgaBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("clicked");
+});
--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
-// const cors = require('cors');
-const path = require('path');
-
-const app = express()
+// const cors = require("cors");
+const path = require("path");
+const axios = require("axios");
+const app = express();
 
 app.use(express.static(path.join(__dirname, "../Client")));
 
 // app.use(cors());
 app.use(express.json());
+// app.get to build the endpoint and determine function
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/../client/index.html"));
+});
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, "/../client/index.html"));
-}); 
+function makeGetRequest(path) {
+    axios.get(path).then(
+        (response) => {
+            var result = response.data;
+            console.log(result);
+        },
+        (error) => {
+            console.log(error);
+        }
+    );
+}
+    
+makeGetRequest("https://api.boardgameatlas.com/oauth/authorize?response_type=code&client_id=4ljazoQBAE&redirect_uri=http://localhost:3000/");
+
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {

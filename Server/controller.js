18cc87c273f93de47be7d35c91e9053b745b2307
Holly McGuinness <gaithelema@gmail.com>
2022-08-10 17:42:43 -0400

    removed external api and added db

--- Server/controller.js
+++ Server/controller.js
@@ -1,8 +1,44 @@
 // table stuff here
-module.exports = {
-  getGames: function (req, res) {
+require("dotenv").config();
+const Sequelize = require("sequelize");
+const { DATABASE_URL } = process.env;
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
 
-    let randomIndex = Math.floor(Math.random() * games.length),
-    let randomGame = games[randomIndex],
-  }
-}
+let globalId = 0;
+
+module.exports = {
+  getWinner: (req, res) => {
+    res.status(200).send(Winners);
+  },
+  createWinner: (req, res) => {
+    console.log(req.body);
+    const { gameTitle, winnerName, score } = req.body;
+    sequelize.query(
+      `INSERT INTO winners (gameTitle, winnerName, score) VALUES ('{gameTitle}','${winnerName}', ${score})`
+    );
+  },
+  seed: (req, res) => {
+    sequelize
+      .query(
+        `drop table if exists winners;
+        create table winners (
+            id serial primary key,
+            gameTitle varchar(255),
+            winnerName varchar(255),
+            score int);
+            `
+      )
+      .then(() => {
+        console.log("DB seeded!");
+        res.sendStatus(200);
+      })
+      .catch((err) => console.log("error seeding DB", err));
+  },
+};
--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,82 @@
-const express = require('express');
-// const cors = require("cors");
+const express = require("express");
+const cors = require("cors");
 const path = require("path");
-const { authBga } = require("./controller");
+const { createWinner, seed } = require("./controller");
 const app = express();
+// const querystring = require("querystring");
+require("dotenv").config();
+const axios = require("axios");
 
+const {
+  // to pull Winner table from database
+  getWinner,
+
+  // to update a winner in the database
+  updateWinner,
+} = require("./controller");
+
+// app.get(`/api/winners`, getWinner);
+app.post(`/api/winners`, createWinner);
+app.post(`/api/seed`, seed);
+// app.put(`/api/winners/:id`, updateWinner);
+
+app.use(express.static("static"));
 app.use(express.static(path.join(__dirname, "../Client")));
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
-// app.get to build the endpoint and determine function
+
+// app.use(cookieParser(process.env.COOKIE_SECRET));
+
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/../client/index.html"));
 });
-app.get("/api/bga", authBga);
 
-app.get("/node_modules/axios/dist/axios.min.js", function (req, res) {
-  res.sendFile(path.join(__dirname, "../node_modules/axios/dist/axios.min.js"));
-});
+// const state = {
+//   rg78qn: {
+//     redirect_uri: "http://localhost:3000/",
+//   },
+// };
 
-const port = process.env.PORT || 3000
+// app.get("/auth", (req, res) => {
+//   // const stateParam = nanoid(8);
+//   // res.cookie("stateParam", stateParam, { maxAge: 1000 * 60 * 5, signed: true });
+//   res.redirect(
+//     `https://api.boardgameatlas.com/oauth/authorize?` +
+//       querystring.stringify({
+//         response_type: "code",
+//         state: process.env.STATE_PARAM,
+//         client_id: process.env.BGA_CLIENT_ID,
+//         redirect_uri: `http://localhost:3000/oauthcallback`,
+//       })
+//   );
+// });
+
+// app.get("/oauthcallback", ({ query: { code } }, res) => {
+//   const body = {
+//     client_id: process.env.BGA_CLIENT_ID,
+//     client_secret: process.env.BGA_CLIENT_SECRET,
+//     code,
+//     redirect_uri: "http://localhost:3000/oauthcallback",
+//     grant_type: "authorization_code",
+//   };
+//   const opts = { headers: { accept: "application/json" } };
+//   axios
+//     .post("https://api.boardgameatlas.com/oauth/token", body, opts)
+//     // .then((_res) => _res.data.access_token)
+//     .then((token) => {
+//       console.log(token);
+//       let uri =
+//         "http://localhost:3000/" || "http://localhost:3000/oauthcallback";
+//       res.redirect(uri);
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ error: err.message });
+//     });
+// });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
